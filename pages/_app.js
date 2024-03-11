8a0b2b046fbad1231881bb6acf0921425131d5ae
youqihuang <youqihua@usc.edu>
2024-03-11 14:40:51 -0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Layout } from '../components';
+import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
+require('dotenv').config();
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  )
 }
+
+export default MyApp
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'a0djjxri',
+    dataset: 'production'
+  }
+})
